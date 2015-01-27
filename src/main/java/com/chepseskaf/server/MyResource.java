3f269d3ecc9679d1d35fed98ac0f140b7cf4bf39
chepseskaf <chepseskaf@gmail.com>
2015-01-27 21:21:08 +0100

    Initial commit

---
+++ src/main/java/com/chepseskaf/server/MyResource.java
@@ -0,0 +1,26 @@
+package com.chepseskaf.server;
+
+import javax.ws.rs.GET;
+import javax.ws.rs.Path;
+import javax.ws.rs.Produces;
+import javax.ws.rs.core.MediaType;
+
+/**
+ * Root resource (exposed at "myresource" path)
+ */
+@Path(MyResource.PATH)
+public class MyResource {
+    final public static String PATH = "myresource";
+
+    /**
+     * Method handling HTTP GET requests. The returned object will be sent
+     * to the client as "text/plain" media type.
+     *
+     * @return String that will be returned as a text/plain response.
+     */
+    @GET
+    @Produces(MediaType.APPLICATION_JSON)
+    public String getIt() {
+        return "Got it";
+    }
+}
